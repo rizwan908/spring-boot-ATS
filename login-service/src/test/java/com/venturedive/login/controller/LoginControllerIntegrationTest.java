cd15642d1ad7a9075d44871f62d72736f1f6c8b6
rizwan ahmed <rizwankhan908@gmail.com>
2022-02-20 00:07:58 +0500

    added login integration test case

--- login-service/src/test/java/com/venturedive/login/controller/LoginControllerIntegrationTest.java
+++ login-service/src/test/java/com/venturedive/login/controller/LoginControllerIntegrationTest.java
@@ -1,20 +1,27 @@
-package com.venturedive.ticket.controller;
+package com.venturedive.login.controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URISyntaxException;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.venturedive.login.utils.JwtUtils;
+
 import reactor.core.publisher.Mono;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class TicketControllerIntegrationTest {
+class LoginControllerIntegrationTest {
 
 	private WebTestClient webTestClient;
+	@Autowired
+	private JwtUtils jwtutils;
 	
 	@BeforeEach
 	public void setup() {
@@ -22,16 +29,18 @@ public class TicketControllerIntegrationTest {
 	}
 
 	@Test
-	public void testGetDeliveryTickets() throws URISyntaxException {
-		/*
-		 * get token from login api and use it to get tickets
-		 */
+	public void testLogin() throws URISyntaxException {
 		EntityExchangeResult<byte[]> entityExchangeResult = webTestClient.post().uri("/login")
 				.body(Mono.just(Map.of("username", "rizwan", "password", "rizwan")), Map.class).exchange().expectStatus()
 				.is2xxSuccessful().expectBody().returnResult();
 		String token = new String(entityExchangeResult.getResponseBody());
-		webTestClient.get().uri("/ticket").header("Authorization", "Bearer " + token).exchange().expectStatus()
-				.is2xxSuccessful();
-		
+		assertTrue(jwtutils.validateToken(token));	
+	}
+
+	@Test
+	public void testInvalidLogin() throws URISyntaxException {
+		webTestClient.post().uri("/login")
+				.body(Mono.just(Map.of("username", "test", "password", "rizwan")), Map.class).exchange().expectStatus()
+				.is4xxClientError();
 	}
 }
---
+++ login-service/src/test/java/com/venturedive/login/controller/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package com.venturedive.login.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
